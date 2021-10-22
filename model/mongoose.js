50e5097009109c456edbb5b488cde8349116c3d6
Ahmed <ahmed.jamil7410@gmail.com>
2021-10-23 01:11:10 +0500

    Made code more sensible

--- model/mongoose.js
+++ model/mongoose.js
@@ -1,26 +1,28 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const userSchema = new mongoose.Schema({
-    firstName : String,
-    lastName : String,
-    email : {
-        type : String,
-        required : true
-    },
-    password : {
-        type: String,
-        required : true
-    },
-    about :String
-
-})
-userSchema.methods.savePass = function(password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString('hex');
-}
-userSchema.methods.validPassword = function(password) { 
-    var hash = crypto.pbkdf2Sync(password,  
-    this.salt, 1000, 64, `sha512`).toString(`hex`); 
-    return this.hash === hash; 
-}; 
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  about: String,
+});
+userSchema.methods.savePass = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString("hex");
+};
+userSchema.methods.validPassword = function (password) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return this.hash === hash;
+};
 module.exports.userModel = mongoose.model("User", userSchema);
